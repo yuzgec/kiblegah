93115789dc8f5b1789dbe81cf64194273f9a218d
yuzgec <olcayy@gmail.com>
2022-02-02 03:47:52 +0300

    Fix

--- app/View/Components/Shop/ProductList.php
+++ app/View/Components/Shop/ProductList.php
@@ -7,16 +7,21 @@ use Illuminate\View\Component;
 
 class ProductList extends Component
 {
-    public $size;
+    public $size = null;
+    public $paginate = null;
+    public $limit = null;
 
-    public function __construct($size = null)
+
+    public function __construct($size,$paginate,$limit )
     {
         $this->size = $size;
+        $this->paginate = $paginate;
+        $this->limit = $limit;
     }
 
     public function render()
     {
-        $Product = Product::with('getCategory')->get();
+        $Product = Product::with('getCategory')->paginate($this->paginate);
         return view('components.shop.product-list', compact('Product'));
     }
 }
--- resources/views/components/shop/product-list.blade.php
+++ resources/views/components/shop/product-list.blade.php
@@ -1,11 +1,10 @@
 @foreach($Product as $item)
-<li class="col-6 {{ (!$size) ? 'col-md-4' :  $size }} product-item">
-
+<li class="col-6 {{ (!$size) ? 'col-md-3' :  $size }} product-item">
     <div class="product-item__outer h-100">
         <div class="product-item__inner px-xl-4 p-3">
             <div class="product-item__body pb-xl-2">
                 <h5 class="mb-1 product-item__title">
-                    <a href="{ route('urun', $item->slug) }}" class="text-blue font-weight-bold">{{ $item->title }}</a>
+                    <a href="{ route('urun', $item->slug) }}" class="text-blue font-weight-bold" title="{{ $item->title }}"> {{ $item->title }}</a>
                 </h5>
                 <div class="mb-2">
                     <a href="{{ route('urun', $item->slug) }}" class="d-block text-center" title="{{ $item->title }}">
@@ -14,7 +13,7 @@
                 </div>
                 <div class="flex-center-between mb-1">
                     <div class="prodcut-price d-flex align-items-center flex-wrap position-relative">
-                        <ins class="font-size-20 text-black text-decoration-none mr-2 font-weight-bold">{{ money($item->price) }}₺ - <del class="font-size-1">{{ money($item->oldu_price) }}</del></ins>
+                        <ins class="font-size-20 text-black text-decoration-none mr-2 font-weight-bold">{{ money($item->price) }}₺ - <del class="font-size-1">{{ money($item->old_price) }}</del></ins>
                     </div>
                     <div class="d-none d-xl-block prodcut-add-cart">
                         <a href="#" class="btn px-2 btn-sm transition-3d-hover"><i class="ec ec-add-to-cart mr-2 font-size-16"></i> Sepete Ekle</a>
