565353179da4c7c293be4a3565fe17527917c587
yuzgec <olcayy@gmail.com>
2022-02-17 12:01:17 +0300

    Fix

--- resources/views/frontend/shop/sonuc.blade.php
+++ resources/views/frontend/shop/sonuc.blade.php
@@ -96,10 +96,10 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '627987337861442');
+        fbq('init', '884071942125527');
         fbq('track', 'PageView');
     </script>
-    <noscript><img height="1" width="1" style="display:none"  src="https://www.facebook.com/tr?id=627987337861442&ev=PageView&noscript=1"/></noscript>
+    <noscript><img height="1" width="1" style="display:none"  src="https://www.facebook.com/tr?id=884071942125527&ev=PageView&noscript=1"/></noscript>
     <script>fbq('track', 'Purchase', {value: 1, currency: 'TRY'});</script>
     @endsection
 @section('content')
