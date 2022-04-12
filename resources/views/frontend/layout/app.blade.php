11c5dc16b48e881c3203d9583dc91cd4c0e01387
yuzgec <olcayy@gmail.com>
2022-04-12 16:31:19 +0300

    Fix

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -18,5 +18,16 @@
         @include('frontend.layout.footer')
         @include('frontend.layout.js')
         @yield('customJS')
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-216042717-1">
+        </script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-216042717-1');
+        </script>
     </body>
 </html>
