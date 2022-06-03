e2d3f062020b95adb7c8f53cce278fd18db38a32
yuzgec <olcayy@gmail.com>
2022-06-03 18:03:08 +0300

    Fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,7 +2,41 @@
 @section('content')
     @include('backend.layout.alert')
 
-    @include('frontend.layout.slider')
+    <div class="container overflow-hidden">
+        <div class="py-2 border-top">
+            <div class="js-slick-carousel u-slick my-1"
+                 data-slides-show="1"
+                 data-slides-scroll="1"
+                 data-arrows-classes="d-none d-lg-inline-block u-slick__arrow-normal u-slick__arrow-centered--y"
+                 data-arrow-left-classes="fa fa-angle-left u-slick__arrow-classic-inner--left z-index-9"
+                 data-arrow-right-classes="fa fa-angle-right u-slick__arrow-classic-inner--right"
+                 data-responsive='[{
+                "breakpoint": 992,
+                "settings": {
+                    "slidesToShow": 1
+                }
+            }, {
+                "breakpoint": 768,
+                "settings": {
+                    "slidesToShow": 1
+                }
+            }, {
+                "breakpoint": 554,
+                "settings": {
+                    "slidesToShow": 1
+                }
+            }]'>
+                @foreach($Slider as $item)
+                    <div class="js-slide">
+                        <a href="{{ $item->button_link }}">
+                            <img class="img-fluid m-auto" src="{{ (!$item->getFirstMediaUrl('page')) ? '/frontend/resimyok.jpg': $item->getFirstMediaUrl('page')}}" alt="{{ config('app.name') }}" style="border-radius: 10px">
+                        </a>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="mb-5">
             <div class="row">
